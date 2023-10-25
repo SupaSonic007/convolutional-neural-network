6804640dec6ae6adefc01219332547fd59597979
joush007 <joshua.kisnorbo@stu.burgmann.act.edu.au>
2023-10-26 10:10:58 +1100

    Setup CNN Base

---
+++ main.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class ConvolutionLayer:
+
+    def __init__(self) -> None:
+        pass
+
+class PoolingLayer:
+
+    def __init__(self) -> None:
+        pass
+
+class FullyConnectedLayer:
+
+    def __init__(self) -> None:
+        pass
+
+class CNN:
+
+    def __init__(self) -> None:
+        pass
